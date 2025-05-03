6b2169e12624280d430756c7691ee59ae3ed1074
Marcono1234 <Marcono1234@users.noreply.github.com>
2025-05-04 01:40:28 +0200

    fix path

--- .github/workflows/process-codeql-sink-models.js
+++ .github/workflows/process-codeql-sink-models.js
@@ -7,7 +7,7 @@
 
 const path = require('node:path')
 const fs = require('node:fs')
-const yaml = require('../../codeql-models-working-dir/yaml')
+const yaml = require('../../codeql-models-working-dir/node_modules/yaml')
 
 /** @type Map<string, string[]> */
 const codeInjectionSinks = new Map()
